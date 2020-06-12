c33e28c4119df964cc77dd8483cb56bdbd2c45a1
Diego Caetano <diego.caetano@outlook.com>
2020-06-12 15:29:59 -0300

    updated

--- app/index.js
+++ app/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-//abcd
+const rp = require('request-promise');
+
 app.get('/', (req, res) => {
+    let buyboxes = null;
+    try {
+        buyboxes = await rp(`http://${process.env.BUYBOX_SERVICE}`);
+    } catch (e) {
+        console.log(e);
+    }
+
     res.json({
         id: 123,
-        name: "MyRandomSeller2",
-        description: "SellerInfo"
+        name: "MyRandomSeller",
+        description: "SellerInfo",
+        buyboxes: buyboxes
     })
 });
 
